1abde7337b3ebbb1608df9c6778fa73b70e4020e
beastehg <beastehg@gmail.com>
2025-08-16 16:41:19 +0300

    Команда dek теперь внутри корзины работает как удалить а везде как перенести в корзину

--- del.cmd
+++ del.cmd
@@ -23,69 +23,119 @@ echo [DEBUG] Атрибуты после снятия:
 attrib "%target%" 2>nul
 
 set "recycle=C:\Корзина"
-if not exist "!recycle!" (
-  echo [DEBUG] Создание папки Корзина: "!recycle!"
-  mkdir "!recycle!" 2>nul
-  if errorlevel 1 (
-    echo Ошибка при создании папки Корзина.
-    exit /b 1
-  )
-)
+set "is_trash=0"
+if /i "%~dp1"=="!recycle!\" set "is_trash=1"
 
-set "name=%~nx1"
-set "orig_path=%~dp1"
-set "timestamp=%date% в %time:~0,8%"
-set "log_file=!recycle!\log.txt"
-
-:: Проверяем, папка ли это
-echo [DEBUG] Проверка: является ли "%target%" папкой...
-if exist "%target%\*" (
-  echo [DEBUG] Это папка
-  :: Рассчитываем размер папки
-  for /f "tokens=3" %%a in ('dir /s /a /-c "%target%" 2^>nul ^| find "File(s)"') do set "size=%%a"
-  move "%target%" "!recycle!\" >nul 2>nul
-  if errorlevel 1 (
-    echo Ошибка при перемещении папки "%target%".
-    exit /b 1
-  )
-  :: Ждем пока папка переместится, максимум 10 попыток по 3 секунды
-  set /a tries=0
-  :wait_move_folder
-  if exist "%target%" (
-    echo [DEBUG] Попытка !tries!: папка существует.
-    if !tries! geq 10 (
-      echo Папка "%target%" все еще существует после перемещения.
+if !is_trash!==1 (
+  :: Permanent delete
+  echo [DEBUG] Проверка: является ли "%target%" папкой...
+  if exist "%target%\*" (
+    echo [DEBUG] Это папка
+    rd /s /q "%target%" 2>nul
+    if errorlevel 1 (
+      echo Ошибка при удалении папки "%target%".
       exit /b 1
     )
-    ping -n 4 localhost >nul
-    set /a tries+=1
-    goto wait_move_folder
+    :: Ждем пока папка удалится, максимум 10 попыток по 3 секунды
+    set /a tries=0
+    :wait_remove_folder
+    if exist "%target%" (
+      echo [DEBUG] Попытка !tries!: папка существует.
+      if !tries! geq 10 (
+        echo Папка "%target%" все еще существует после удаления.
+        exit /b 1
+      )
+      ping -n 4 localhost >nul
+      set /a tries+=1
+      goto wait_remove_folder
+    )
+  ) else (
+    echo [DEBUG] Это файл
+    del /f /q "%target%" 2>nul
+    if errorlevel 1 (
+      echo Ошибка при удалении файла "%target%".
+      exit /b 1
+    )
+    :: Ждем пока файл удалится, максимум 10 попыток по 3 секунды
+    set /a tries=0
+    :wait_remove_file
+    if exist "%target%" (
+      echo [DEBUG] Попытка !tries!: файл существует.
+      if !tries! geq 10 (
+        echo Файл "%target%" все еще существует после удаления.
+        exit /b 1
+      )
+      ping -n 4 localhost >nul
+      set /a tries+=1
+      goto wait_remove_file
+    )
   )
 ) else (
-  echo [DEBUG] Это файл
-  :: Рассчитываем размер файла
-  for /f "tokens=3" %%a in ('dir /a /-c "%target%" 2^>nul ^| find "File(s)"') do set "size=%%a"
-  move "%target%" "!recycle!\" >nul 2>nul
-  if errorlevel 1 (
-    echo Ошибка при перемещении файла "%target%".
-    exit /b 1
-  )
-  :: Ждем пока файл переместится, максимум 10 попыток по 3 секунды
-  set /a tries=0
-  :wait_move_file
-  if exist "%target%" (
-    echo [DEBUG] Попытка !tries!: файл существует.
-    if !tries! geq 10 (
-      echo Файл "%target%" все еще существует после перемещения.
+  :: Move to recycle
+  if not exist "!recycle!" (
+    echo [DEBUG] Создание папки Корзина: "!recycle!"
+    mkdir "!recycle!" 2>nul
+    if errorlevel 1 (
+      echo Ошибка при создании папки Корзина.
       exit /b 1
     )
-    ping -n 4 localhost >nul
-    set /a tries+=1
-    goto wait_move_file
   )
-)
 
-:: Добавляем запись в лог с пробелами
-echo !name!  ^|  !timestamp!  ^|  !orig_path!  ^|  !size! >> "!log_file!"
+  set "name=%~nx1"
+  set "orig_path=%~dp1"
+  set "timestamp=%date% в %time:~0,8%"
+  set "log_file=!recycle!\log.txt"
+
+  :: Проверяем, папка ли это
+  echo [DEBUG] Проверка: является ли "%target%" папкой...
+  if exist "%target%\*" (
+    echo [DEBUG] Это папка
+    :: Рассчитываем размер папки
+    for /f "tokens=3" %%a in ('dir /s /a /-c "%target%" 2^>nul ^| find "File(s)"') do set "size=%%a"
+    move "%target%" "!recycle!\" >nul 2>nul
+    if errorlevel 1 (
+      echo Ошибка при перемещении папки "%target%".
+      exit /b 1
+    )
+    :: Ждем пока папка переместится, максимум 10 попыток по 3 секунды
+    set /a tries=0
+    :wait_move_folder
+    if exist "%target%" (
+      echo [DEBUG] Попытка !tries!: папка существует.
+      if !tries! geq 10 (
+        echo Папка "%target%" все еще существует после перемещения.
+        exit /b 1
+      )
+      ping -n 4 localhost >nul
+      set /a tries+=1
+      goto wait_move_folder
+    )
+  ) else (
+    echo [DEBUG] Это файл
+    :: Рассчитываем размер файла
+    for /f "tokens=3" %%a in ('dir /a /-c "%target%" 2^>nul ^| find "File(s)"') do set "size=%%a"
+    move "%target%" "!recycle!\" >nul 2>nul
+    if errorlevel 1 (
+      echo Ошибка при перемещении файла "%target%".
+      exit /b 1
+    )
+    :: Ждем пока файл переместится, максимум 10 попыток по 3 секунды
+    set /a tries=0
+    :wait_move_file
+    if exist "%target%" (
+      echo [DEBUG] Попытка !tries!: файл существует.
+      if !tries! geq 10 (
+        echo Файл "%target%" все еще существует после перемещения.
+        exit /b 1
+      )
+      ping -n 4 localhost >nul
+      set /a tries+=1
+      goto wait_move_file
+    )
+  )
+
+  :: Добавляем запись в лог с пробелами
+  echo !name!  ^|  !timestamp!  ^|  !orig_path!  ^|  !size! >> "!log_file!"
+)
 
 exit /b 0
